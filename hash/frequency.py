4ab467ca8e52f4f8d0b6363f1b857ef9fa3250fe
mandeukJeong <wjdalstj137@gmail.com>
2024-04-17 18:28:20 +0900

    hash - 빈도수 ver 1(ms)

---
+++ hash/frequency.py
@@ -0,0 +1,21 @@
+def solution(nums):
+    answer = -1
+    frequency = dict()
+
+    for x in nums:
+        if x in frequency:
+            frequency[x] += 1
+        else:
+            frequency[x] = 1
+
+    for key in frequency:
+        if frequency[key] == 1 and key > answer:
+            answer = key
+       
+    return answer            
+                
+print(solution([3, 9, 2, 12, 9, 12, 8, 7, 9, 12]))
+print(solution([2, 1, 3, 2, 1, 3, 4, 5, 4, 5, 6, 7, 6 ,7, 8, 8]))
+print(solution([23, 56, 11, 67, 120, 560, 812, 960, 560, 777, 888, 960]))
+print(solution([11, 73, 156, 789, 345, 156, 789, 345, 678, 555, 678]))
+print(solution([1, 3, 1, 5, 7, 2, 3, 1, 5]))
