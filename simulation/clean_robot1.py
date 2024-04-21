687b34001f9a05fd86c1530abb96c85525248b24
mandeukJeong <wjdalstj137@gmail.com>
2024-04-22 01:40:29 +0900

    simulation - 청소 로봇 ver 1 리팩토링

--- simulation/clean_robot1.py
+++ simulation/clean_robot1.py
@@ -2,20 +2,13 @@ def solution(moves):
     x = y = 0
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
+    dir = ['U', 'R', 'D', 'L']
 
     for c in moves:
-        if c == 'U':
-            x = x + dx[0]
-            y = y + dy[0]
-        elif c == 'R':
-            x = x + dx[1]
-            y = y + dy[1]
-        elif c == 'D':
-            x = x + dx[2]
-            y = y + dy[2]
-        elif c == 'L':
-            x = x + dx[3]
-            y = y + dy[3]
+        for k in range(4):
+            if c == dir[k]:
+                x = x + dx[k]
+                y = y + dy[k]
     
     return [x, y]
                             
