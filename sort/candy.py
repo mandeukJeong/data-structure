b9d19d975778be65226e840954880e0cbe883aba
mandeukJeong <wjdalstj137@gmail.com>
2024-05-07 14:09:04 +0900

    sort - 사탕의 종류(ms)

--- sort/candy.py
+++ sort/candy.py
@@ -1,12 +1,18 @@
 def solution(nums):
-    n = len(nums)
-    cnt = 1
+    answer = 0
+    tmp = 0
     nums.sort()
-    for i in range(1, n):
-        if nums[i-1] != nums[i]:
-            cnt += 1
+    pos = len(nums) // 2
+
+    for x in nums:
+        if x != tmp:
+            tmp = x
+            answer += 1
+
+    if pos < answer:
+        answer = pos
             
-    return cnt if cnt < n//2 else n//2
+    return answer
     
                        
 print(solution([2, 1, 1, 3, 2, 3, 1, 2]))
