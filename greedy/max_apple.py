a6e9c8001ab262d95672ad73ecd0647f29eefde4
mandeukJeong <wjdalstj137@gmail.com>
2024-05-08 00:37:24 +0900

    Greedy - 최대 사과의 개수(ms)

--- greedy/max_apple.py
+++ greedy/max_apple.py
@@ -1,15 +1,19 @@
 def solution(box, limit):
     answer = 0
-    box.sort(key = lambda v : -v[1])
+    box.sort(key = lambda v : (-v[1], -v[0]))
+
     for x in box:
-        cnt = min(limit, x[0])
-        answer += (cnt * x[1])
-        limit -= cnt
         if limit == 0:
             break
-    
+
+        if limit >= x[0]:
+            answer += x[0] * x[1]
+            limit -= x[0]
+        else:
+            answer += limit * x[1]
+            limit = 0
+               
     return answer
-    
                                            
 print(solution([[2, 20], [2, 10], [3, 15], [2, 30]], 5))
 print(solution([[1, 50], [2, 20], [3, 30], [2, 31], [5, 25]], 10))
