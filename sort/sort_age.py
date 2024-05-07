09d08e6c14a83083a1def8fa19bb9d0dc18002b6
mandeukJeong <wjdalstj137@gmail.com>
2024-05-07 15:40:07 +0900

    sort - 나이순 정렬: 백준 10814

--- sort/sort_age.py
+++ sort/sort_age.py
@@ -1,15 +1,13 @@
 def solution(member):
-    member.sort(key = lambda v : v[0])
-    
+    member.sort(key = lambda v : (v[0]))
     for x in member:
         print(x[0], x[1])
 
+
 n = int(input())
 arr = []
-
 for i in range(n):
     a, b = input().split()
     a = int(a)
     arr.append([a, b])
-
 solution(arr)
