985e60da395915c64c3cce2a169a359c2ac385b2
mandeukJeong <wjdalstj137@gmail.com>
2024-05-08 18:14:22 +0900

    Binary Search - 고정된 숫자

--- binary_search/fixed_number.py
+++ binary_search/fixed_number.py
@@ -1,20 +1,18 @@
 def solution(nums):
-    answer = -1
     left = 0
     right = len(nums) - 1
-
     while left <= right:
         mid = (left + right) // 2
-
         if nums[mid] == mid:
-            return mid
-        elif nums[mid] < mid:
+            return nums[mid]
+        if nums[mid] < mid:
             left = mid + 1
         else:
             right = mid - 1
     
-    return answer    
+    return -1    
      
+    
 print(solution([-3, -2, 0, 1, 3, 5, 8, 9, 12]))
 print(solution([-10, -5, -2, 3, 4, 6, 7, 8]))
 print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9]))
