1fa9f0b48fd9ac4c3b061c554f8a194a15e9317e
mandeukJeong <wjdalstj137@gmail.com>
2024-05-08 16:18:15 +0900

    Greedy - 카드 점수(O(n))

--- greedy/card_score.py
+++ greedy/card_score.py
@@ -1,17 +1,21 @@
 def solution(nums, k):
-    answer = 0
-    n = len(nums)
-    for i in range(k+1):
-        sumN = 0
-        for j in range(i):
-            sumN += nums[j]
-        for j in range(n-k+i, n):
-            sumN += nums[j]
-        answer = max(answer, sumN)
-  
-    return answer
-        
-                                            
+    total = sum(nums)
+    m = len(nums) - k
+    score = 0
+
+    for i in range(m):
+        score += nums[i]
+
+    minS = score
+    left = 0
+
+    for right in range(m, len(nums)):
+        score += (nums[right] - nums[left])
+        left += 1
+        minS = min(minS, score)
+
+    return total - minS
+                       
 print(solution([2, 3, 7, 1, 2, 1, 5], 4))
 print(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5))
 print(solution([1, 30, 3, 5, 6, 7], 3))
