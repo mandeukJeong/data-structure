698484fd65b4e22168e858b392220b799461ec53
mandeukJeong <wjdalstj137@gmail.com>
2024-05-08 17:22:16 +0900

    Binary Search - 이진 탐색

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,20 +1,16 @@
 def solution(nums, target):
-    answer = -1
     left = 0
     right = len(nums) - 1
-
     while left <= right:
         mid = (left + right) // 2
-
-        if nums[mid] == target:
-            answer = mid
-            return answer
-        elif nums[mid] > target:
-            right = mid - 1
-        else:
+        if target == nums[mid]:
+            return mid
+        if target > nums[mid]:
             left = mid + 1
+        else:
+            right = mid - 1
 
-    return answer
+    return -1
                                                  
 print(solution([2, 5, 7, 8, 10, 15, 20, 24, 25, 30], 8))
 print(solution([-3, 0, 2, 5, 8, 9, 12, 15], 0))
