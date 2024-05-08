8a9f2be6f909d05fbb8737cbcc0307c4ecb7f18b
mandeukJeong <wjdalstj137@gmail.com>
2024-05-08 17:59:31 +0900

    Binary Search - 트럭 찾기

--- binary_search/find_truck.py
+++ binary_search/find_truck.py
@@ -1,9 +1,16 @@
-from bisect import bisect_left
-
 def solution(nums, weight):
-    answer = bisect_left(nums, weight)
+    left = 0
+    right = len(nums)
 
-    return -1 if answer == len(nums) else answer
+    while left < right:
+        mid = (left + right) // 2
+
+        if weight > nums[mid]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return -1 if right == len(nums) else right
     
 print(solution([100, 120, 150, 160, 165, 170, 175, 180, 190, 200], 170))                                          
 print(solution([200, 250, 260, 265, 275, 290, 300, 325, 350, 370], 270))
