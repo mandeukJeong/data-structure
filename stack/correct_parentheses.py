0b09bcd550b4de2141583b2e4d6850b118a52139
mandeukJeong <wjdalstj137@gmail.com>
2024-05-23 22:04:57 +0900

    Stack - 올바른 괄호(ms)

--- stack/correct_parentheses.py
+++ stack/correct_parentheses.py
@@ -1,19 +1,17 @@
 def solution(s):
     answer = "YES"
     stack = []
+
     for x in s:
-        if x == ')':
+        if x == "(":
+            stack.append(x)
+        else:
             if len(stack) == 0:
                 return "NO"
-            stack.pop()
-        else:
-            stack.append(x)
+            else:
+                stack.pop()
 
-    if len(stack) > 0:
-        return "NO"
-
-    return answer
-
+    return answer if len(stack) == 0 else "NO"
 
 print(solution("((()))()"))
 print(solution("(()(()"))
