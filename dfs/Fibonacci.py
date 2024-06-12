ae61798709f9d777825068c2a6eb9a3a96233cd1
mandeukJeong <wjdalstj137@gmail.com>
2024-06-12 23:37:28 +0900

    DFS - 피보나치 수열(ms)

--- dfs/Fibonacci.py
+++ dfs/Fibonacci.py
@@ -1,8 +1,11 @@
 def DFS(n):
     if n == 1 or n == 2:
         return 1
-    else:
-        return DFS(n-2) + DFS(n-1)
-              
+    elif n >= 3:
+        return DFS(n - 1) + DFS(n - 2)
+                  
 print(DFS(5))
+print(DFS(6))
+print(DFS(8))
+print(DFS(10))
 
