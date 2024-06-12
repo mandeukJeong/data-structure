32cdd93959f39cfa577757bd38ee2336b9b6405e
mandeukJeong <wjdalstj137@gmail.com>
2024-06-12 23:01:56 +0900

    DFS - n! 구하기(ms)

--- dfs/factorial.py
+++ dfs/factorial.py
@@ -2,7 +2,9 @@ def DFS(n):
     if n == 1:
         return 1
     else:
-        return n * DFS(n-1)
+        return n * DFS(n - 1)
 
 print(DFS(5))             
 print(DFS(6))
+print(DFS(7))
+print(DFS(8))
